846355bc1e16b2427f399a7fd75691a1a802b1e4
Sanskriti singh <146508510+Sanskriti3104@users.noreply.github.com>
2025-04-14 11:58:44 +0530

    Add Simon-Game logic with level and flash effect

--- app.js
+++ app.js
@@ -1,8 +1,88 @@
+const statusText = document.getElementById("status-text");
+const colorButtons = document.querySelectorAll(".color-button");
+const scoreBoard = document.getElementById("score-value");
 
-document.querySelectorAll('.color-button').forEach(button => {
-    button.addEventListener('click', () => {
-        button.classList.remove('flash'); // Remove if already exists (restarts animation)
-        void button.offsetWidth; // Force reflow to allow re-adding class
-        button.classList.add('flash');
-    });
+let startGame = false;
+let level = 0;
+let randomColorPattern = [];
+let userPattern = [];
+let colors = ['red','green','blue','yellow'];
+
+// Add click listener for all buttons
+colorButtons.forEach(button => {
+    button.addEventListener('click',handleUserClick)
 });
+
+function handleUserClick(event){
+    //Start the game on first click
+    if(!startGame){
+        startGame = true;
+        nextSequence();
+        return;
+    }
+
+    const userChosenColor = event.target.id;
+    userPattern.push(userChosenColor);
+    flashButton(userChosenColor);
+    console.log("User Chosen Color : " + userChosenColor);
+
+    checkPattern(userPattern.length - 1);
+}
+
+function nextSequence(){
+    userPattern = [];
+    level++;
+    statusText.innerHTML = "LEVEL" + level;
+
+    let randomColor = randomColorGenerator();
+    randomColorPattern.push(randomColor);
+
+    // Flash each color in the pattern with delay
+    let i=0;
+    const interval = setInterval(() => {
+        flashButton(randomColorPattern[i]);
+        console.log("Generated color : " + randomColorPattern[i]);
+        i++;
+        if(i >= randomColorPattern.length){
+            clearInterval(interval);
+        }
+    },600);
+}
+
+// Generate one random color
+function randomColorGenerator(){
+    const randomIndex = Math.floor(Math.random() * colors.length);
+    return colors[randomIndex];
+}
+
+// Flash the button
+function flashButton(color){
+    const button = document.getElementById(color);
+    button.classList.remove('flash');
+    void button.offsetWidth; // Force reflow
+    button.classList.add('flash');
+    setTimeout(()=>{
+        button.classList.remove('flash');
+    },300);
+}
+
+// Check user's current input against the pattern
+function checkPattern(currentIndex){
+    if(userPattern[currentIndex] === randomColorPattern[currentIndex]){
+        if(userPattern.length == randomColorPattern.length){
+            setTimeout(nextSequence,1000);
+        }
+    }else{
+        gameOver();
+    }
+}
+
+// Game Over logic
+function gameOver() {
+    statusText.innerHTML = "Game Over!";
+    document.getElementById("game-over-popup").classList.remove("hidden");
+    startGame = false;
+    level = 0;
+    randomColorPattern = [];
+    userPattern =[];
+}
